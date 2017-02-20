47fdf9b9fdece2aae19fedd502bb71ee88763c38
MrPickles2009 <MrPickles2009@users.noreply.github.com>
2017-02-20 09:39:06 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+var leapyear;
+/*
+//older way
+function isLeapYear() {
+
+}
+
+//newer way
+isLeapYear = function() {
+
+};
+*/
+//modern way
+leapyear = () => {
+
+};
+
+module.exports = leapyear;
